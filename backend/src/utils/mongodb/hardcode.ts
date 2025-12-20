7fe2e382b071736260f35daa4487bab1904316f6
crsd <vovalitvinov123lalala@gmail.com>
2025-12-20 14:35:09 +0200

    feat: newprovider price

--- backend/src/utils/mongodb/hardcode.ts
+++ backend/src/utils/mongodb/hardcode.ts
@@ -39,6 +39,7 @@ const CONFIGS = {
       '13e6fa90-c986-4440-8c94-f36f41e22d36': 1208,
       '37f3a270-e312-4a07-8277-4f49cd8025f0': 1300,
       '951bde3a-e48f-412c-9bea-e8fdd4cdd0c7': 1142,
+      'f770ef11-3886-4d27-9327-5a43f120ee4f': 950,
     },
 
     propertyIds: {
